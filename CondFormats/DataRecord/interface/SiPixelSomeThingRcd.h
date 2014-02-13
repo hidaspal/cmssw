6d2acd14f7816e5f202f54b6f1324a3f166ef051
hidaspal <hidas.pal@wigner.mta.hu>
2014-02-13 22:27:29 +0100

    something

---
+++ CondFormats/DataRecord/interface/SiPixelSomeThingRcd.h
@@ -0,0 +1,26 @@
+#ifndef DataRecord_SiPixelSomeThingRcd_h
+#define DataRecord_SiPixelSomeThingRcd_h
+// -*- C++ -*-
+//
+// Package:     DataRecord
+// Class  :     SiPixelSomeThingRcd
+// 
+/**\class SiPixelSomeThingRcd SiPixelSomeThingRcd.h CondFormats/DataRecord/interface/SiPixelSomeThingRcd.h
+
+ Description: <one line class summary>
+
+ Usage:
+    <usage>
+
+*/
+//
+// Author:      
+// Created:     Mon Jun 25 15:48:11 CEST 2007
+// $Id$
+//
+
+#include "FWCore/Framework/interface/EventSetupRecordImplementation.h"
+
+class SiPixelSomeThingRcd : public edm::eventsetup::EventSetupRecordImplementation<SiPixelSomeThingRcd> {};
+
+#endif
